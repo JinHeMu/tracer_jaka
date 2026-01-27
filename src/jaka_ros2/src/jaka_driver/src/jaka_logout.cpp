3a51bd369be504c1729ccfc7e21ec7e3a9dfa2d6
Jinhemu <490754775@qq.com>
2026-01-27 14:25:49 +0800

    revise tcp_link

--- src/jaka_ros2/src/jaka_driver/src/jaka_logout.cpp
+++ src/jaka_ros2/src/jaka_driver/src/jaka_logout.cpp
@@ -14,6 +14,9 @@ int main(void)
     string default_ip = "10.5.5.100";
     robot.login_in(default_ip.c_str());
     robot.disable_robot();
+    robot.power_off();
+    robot.shut_down();
+
     std::cout << "robot disable" << std::endl;
     return 0;
 }
